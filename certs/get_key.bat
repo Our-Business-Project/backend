3256b2b43efcc76ad94abd28fd873c0b538e2125
FirebenderYT <FirebenderYT@gmail.com>
2023-10-22 01:17:34 +0300

    fix: * Removed server variable with certs

--- certs/get_key.bat
+++
@@ -1,4 +0,0 @@
-openssl genpkey -algorithm RSA -out server.key
-openssl req -new -key server.key -out server.csr
-openssl req -x509 -sha256 -days 365 -key server.key -in server.csr -out server.crt
-rm server.csr
--- index.js
+++ index.js
@@ -1,33 +1,19 @@
 import express from "express";
-import https from "https";
-import fs from "fs";
 import dotenv from "dotenv";
 
 import { initRoutes } from "./src/routes/index.js";
-import { checkAppBeforeStart, gracefulShutdown } from "./src/appHelpers.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-const httpsOptions = {
-  key: fs.readFileSync("./certs/server.key"),
-  cert: fs.readFileSync("./certs/server.crt"),
-};
-const server = https.createServer(httpsOptions, app);
-
-checkAppBeforeStart();
-
-const port = process.env.PORT || 8443;
+const port = process.env.PORT || 8000;
 
 initRoutes(app);
 
-server.listen(port, () => {
-  console.log(`App listening at https://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
 });
 
-process.on("SIGTERM", () => gracefulShutdown(server));
-process.on("SIGINT", () => gracefulShutdown(server));
-
-export { app, server };
+export { app };
