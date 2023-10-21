4f3fb21121d646020457ec0f58a84e89312b513e
FirebenderYT <FirebenderYT@gmail.com>
2023-10-21 16:16:07 +0300

    feat: created index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import http from "http";
+import dotenv from "dotenv";
+
+import { initRoutes } from "./src/routes/routes";
+import { checkAppBeforeStart, gracefulShutdown } from "./appHelopers";
+
+dotenv.config();
+
+const app = express();
+
+const server = http.createServer(app);
+
+checkAppBeforeStart();
+
+app.use(express.json());
+
+initRoutes(app);
+
+server.listen(process.env.LISTEN_PORT, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+
+process.on("SIGTERM", () => gracefulShutdown(server));
+process.on("SIGINT", () => gracefulShutdown(server));
+
+export { app, server };
